94b2076b03a22b1b0f7e1362ffe8e6a1dec74ada
Kmilo9999 <k.diaz99@gmail.com>
2023-07-22 22:37:29 -0400

    some changes

--- Assets/Scripts/SpawnBlockTerrain.cs
+++ Assets/Scripts/SpawnBlockTerrain.cs
@@ -8,30 +8,29 @@ public class SpawnBlockTerrain : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] public GameObject[] terrainColumnPrefabs;
     private float currenCounterTime = 0;
-    private Renderer renderComponent;
     private int numBlocks = 0;
     private float currentPosition = 0f;
     private Vector3 worldLowerRight;
     private Vector3 worldLowerLeft;
     private bool[] columsOnScreen;
-    private int currentBlocks = 0;
+   
     void Start()
     {
-        //renderComponent = terrainColumnPrefab.GetComponent<Renderer>();
+        
         worldLowerRight = Camera.main.ViewportToWorldPoint(Vector3.zero);
         worldLowerLeft = Camera.main.ViewportToWorldPoint(new Vector3(1.0f,0.0f,0.0f));
         var distance = worldLowerLeft.x - worldLowerRight.x;
         numBlocks = (int) (distance / 0.5);
         columsOnScreen = Enumerable.Repeat(false, numBlocks).ToArray();
         currentPosition = worldLowerRight.x;
-        //Debug.Log(numBlocks);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         currenCounterTime += Time.deltaTime;
-        if (currenCounterTime > 2)
+        if (currenCounterTime > 3)
         {
             createBlock();
             currenCounterTime = 0;
@@ -58,11 +57,14 @@ public class SpawnBlockTerrain : MonoBehaviour
             worldPosition = ((randomPosition - 1) / 2) + 0.5f;
         }
         
-        var blockPosition = new Vector3(worldLowerRight.x + worldPosition + 0.25f, worldLowerRight.y + 0.25f, 0);
-        
-        var columnType = Random.Range(0, terrainColumnPrefabs.Length);
-        GameObject.Instantiate(terrainColumnPrefabs[columnType], blockPosition, new Quaternion());
+        var blockPosition = new Vector3(worldLowerRight.x + worldPosition + 0.25f, worldLowerRight.y + 0.25f, 0); 
+        spawnColumn(Random.Range(0, terrainColumnPrefabs.Length), blockPosition);
         columsOnScreen[randomPosition] = true;
         
+    }
+
+    private void spawnColumn(int columnType, Vector3 worldPosition)
+    {
+        GameObject.Instantiate(terrainColumnPrefabs[columnType], worldPosition, new Quaternion());
     }
 }
